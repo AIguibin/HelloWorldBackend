8f283aa5b84d46bce5c212b9e755e0a34b2177b1
AIguibin <aiguibin@163.com>
2021-05-31 00:29:20 +0800

    常用工具类或POJO,BEANS,RESULTDTO

---
+++ src/main/java/com/aiguibin/common/beans/JSONParams.java
@@ -0,0 +1,13 @@
+package com.aiguibin.common.beans;
+
+public class JSONParams {
+    private Object data;
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
