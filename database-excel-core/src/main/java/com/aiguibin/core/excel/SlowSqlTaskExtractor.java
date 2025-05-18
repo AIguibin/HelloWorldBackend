f291bbde901949a523eabeaa246fdd27badbcff0
AIguibin <aiguibin@163.com>
2025-05-18 16:24:40 +0800

    add SlowSqlTaskExtractor

--- database-excel-core/src/main/java/com/aiguibin/core/excel/SlowSqlTaskExtractor.java
+++ database-excel-core/src/main/java/com/aiguibin/core/excel/SlowSqlTaskExtractor.java
@@ -5,6 +5,8 @@ import com.aiguibin.core.common.FileAccessor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
@@ -13,9 +15,23 @@ import java.nio.file.Path;
 public class SlowSqlTaskExtractor {
     //日志声明
     private static final Log logger = LogFactory.getLog(FileAccessor.class);
+    // 数据库设计文档路径
+    private static final String TABLE_LIST_DIR="database-excel-core/docs/tableList";
+    /**
+     *
+     * @return
+     */
+    public void tabbleListIsExist(){
 
-    public Path tabbleListIsExist(){
-        Path tableListPath = FileAccessor.getProjectRootFolderPath("database-excel-core/docs/tableList");
-        return tableListPath;
+        Path tableListPath = FileAccessor.getProjectRootFolderPath(TABLE_LIST_DIR);
+        if (Files.notExists(tableListPath)) {
+            try {
+                Files.createDirectories(tableListPath);
+            } catch (IOException e) {
+                logger.info("目录创建失败：\n\r",e);
+            }
+            logger.info("目录已创建: " + TABLE_LIST_DIR);
+        }
+        logger.info(tableListPath.toString());
     }
 }
