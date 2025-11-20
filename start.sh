e3c5e535d692e5a76cd21b539f520963bc90fd13
AIguibin <aiguibin@163.com>
2025-11-20 23:21:50 +0800

    chore: 更新start.sh中的Maven配置文件路径
    添加新的Maven配置文件路径配置，保留原有配置作为备选

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 # 企业开发常用命令
 mvn -gs "D:\Maven\settings-aiguibin.xml" -Dmaven.repo.local="E:\Repository\Local" -T 1C clean package -DskipTests -U -Dmaven.compile.fork=true -Dmaven.test.skip=true && java -jar target/aiguibin-online-table.jar
+mvn -gs "D:\Maven\config\settings.xml" -Dmaven.repo.local="E:\Repository\Local" -T 1C clean package -DskipTests -U -Dmaven.compile.fork=true -Dmaven.test.skip=true && java -jar target/aiguibin-online-table.jar
